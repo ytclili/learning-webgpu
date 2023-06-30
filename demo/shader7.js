b1170dd2fca2aee736016c703599083158dff3f3
孙一飞 <sunyifei@paraview.cn>
2023-06-30 16:24:20 +0800

    webgpu 创建立方体

---
+++ demo/shader7.js
@@ -0,0 +1,47 @@
+// 顶点着色器   
+const vertex = /* wgsl */ `
+struct Uniforms {
+  modelViewProjectionMatrix : mat4x4<f32>,
+}
+@binding(0) @group(0) var<uniform> uniforms : Uniforms;
+
+struct VertexOutput {
+  @builtin(position) Position : vec4<f32>,
+  @location(0) fragUV : vec2<f32>,
+  @location(1) fragPosition: vec4<f32>,
+}
+
+@vertex
+fn main(
+  @location(0) position : vec4<f32>,
+  @location(1) uv : vec2<f32>
+) -> VertexOutput {
+  var output : VertexOutput;
+  output.Position = uniforms.modelViewProjectionMatrix * position;
+  output.fragUV = uv;
+  output.fragPosition = 0.5 * (position + vec4(1.0, 1.0, 1.0, 1.0));
+  return output;
+}
+
+`
+
+const fragment = /* wgsl */ ` 
+@fragment
+fn main(
+  @location(0) fragUV: vec2<f32>,
+  @location(1) fragPosition: vec4<f32>
+) -> @location(0) vec4<f32> {
+  return fragPosition;
+}
+
+`
+
+export {
+    vertex,
+    fragment
+}
+
+
+
+
+// 顶点 -> 顶点着色器  -> 图元装配 => 光栅化 => > 片元着色器 => 深度测试 => 像素写入
